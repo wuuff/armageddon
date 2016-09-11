0699b6e92464b4bae6b29a2396deaeab59d290dc
wuuff <wuuuufff@gmail.com>
2016-09-11 07:07:15 -0500

    Added drawing cities, launchers, and basic target cursor movement

--- AGEDDON/AGEDDON.ino
+++ AGEDDON/AGEDDON.ino
@@ -4,6 +4,35 @@
 #include <Gamebuino.h>
 Gamebuino gb;
 
+const byte city[] PROGMEM = {8,8,
+B00000000,
+B00000000,
+B00000000,
+B00000000,
+B00100010,
+B01110110,
+B01111110,
+B11111111,
+};
+
+const byte launcher[] PROGMEM = {8,8,
+B00011000,
+B00011000,
+B00011000,
+B00100100,
+B00100100,
+B01000010,
+B01000010,
+B10000001,
+};
+
+
+
+// State variables
+uint8_t targetX;
+uint8_t targetY;
+
+
 void setup() {
   gb.begin();
   gb.titleScreen(F("Armageddon"));
@@ -11,8 +40,51 @@ void setup() {
   gb.pickRandomSeed();
 }
 
+void drawCursor(){
+  gb.display.drawFastHLine(targetX-1,targetY,3);
+  gb.display.drawFastVLine(targetX,targetY-1,3);
+}
+
+void drawCities(){
+  for(uint8_t i = 0; i < 8; i++){
+      if( i == 2 || i == 5 ){
+        gb.display.drawBitmap(i*10+2,40,launcher);
+      }else{
+        gb.display.drawBitmap(i*10+2,40,city);
+      }
+    }
+}
+
 void loop() {
   if(gb.update()){
-    gb.display.print("Hello world");
+    //Player input
+    if( gb.buttons.pressed(BTN_A) ){
+      
+    }
+    if( gb.buttons.pressed(BTN_B) ){
+      
+    }
+    
+    if( gb.buttons.repeat(BTN_LEFT,1) ){
+      targetX = targetX-2 > 0 ? targetX-2 : 0;
+    }
+    if( gb.buttons.repeat(BTN_RIGHT,1) ){
+      targetX = targetX+2 < 84 ? targetX+2 : 84;
+    }
+    if( gb.buttons.repeat(BTN_UP,1) ){
+      targetY = targetY-2 > 0 ? targetY-2 : 0;
+    }
+    if( gb.buttons.repeat(BTN_DOWN,1) ){
+      targetY = targetY+2 < 48 ? targetY+2 : 48;
+    }
+
+
+    //Game logic
+
+
+    //Drawing
+    drawCursor();
+    drawCities();
+    
   }
 }
