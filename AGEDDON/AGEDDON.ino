a96db80be825302b20473fd2e1ee96f9ad2fcc33
wuuff <wuuuufff@gmail.com>
2016-09-11 05:43:21 -0500

    Added dummy hello world game

---
+++ AGEDDON/AGEDDON.ino
@@ -0,0 +1,18 @@
+#include <SPI.h>
+//Use for PROGMEM
+#include <avr/pgmspace.h>
+#include <Gamebuino.h>
+Gamebuino gb;
+
+void setup() {
+  gb.begin();
+  gb.titleScreen(F("Armageddon"));
+  gb.battery.show = false;
+  gb.pickRandomSeed();
+}
+
+void loop() {
+  if(gb.update()){
+    gb.display.print("Hello world");
+  }
+}
