ce6893612a2e95b786e5288685344e98ee21e46c
wuuff <wuuuufff@gmail.com>
2016-09-13 10:21:09 -0500

    Added enemy missiles
    Enemy missiles can be destroyed by player detonations, but they
    cannot destroy cities or launchers yet.

--- AGEDDON/AGEDDON.ino
+++ AGEDDON/AGEDDON.ino
@@ -30,10 +30,13 @@ B10000001,
 #define LAUNCHER_ONE 1
 #define LAUNCHER_TWO 2
 #define MAX_PMISSILES 10
-#define PSPEED 3
+#define PSPEED 4
 #define PRADIUS 6
 #define EXPAND 0
 #define SHRINK 1
+
+#define MAX_EMISSILES 10
+#define MAX_CHANCE 50
 
 // State variables
 uint8_t oddLoop = 0;
@@ -42,6 +45,12 @@ uint8_t targetY;
 uint8_t pDests[MAX_PMISSILES][2] = {{100,100},{100,100},{100,100},{100,100},{100,100},{100,100},{100,100},{100,100},{100,100},{100,100}};
 float pMissiles[MAX_PMISSILES][3] = {{100,100,0},{100,100,0},{100,100,0},{100,100,0},{100,100,0},{100,100,0},{100,100,0},{100,100,0},{100,100,0},{100,100,0}};
 uint8_t pDetonations[MAX_PMISSILES][4] = {{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0},{100,100,0,0}};
+
+uint8_t etotal = 5;
+uint8_t echance = 1;
+float espeed = 0.2;
+uint8_t eDests[MAX_EMISSILES] = {100,100,100,100,100,100,100,100,100,100};
+float eMissiles[MAX_EMISSILES][4] = {{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100},{100,100,100,100}};
 
 void setup() {
   gb.begin();
@@ -73,6 +82,7 @@ void drawCities(){
 }
 
 void drawMissiles(){
+  //Player Missiles
   for(uint8_t i = 0; i < MAX_PMISSILES; i++){
     //Check for a valid destination without a current detonation
     if( pDests[i][0] <= 84 && pDetonations[i][0] > 84 ){
@@ -81,6 +91,14 @@ void drawMissiles(){
       }else{
         gb.display.drawLine(56,40,pMissiles[i][0], pMissiles[i][1]);
       }
+    }
+  }
+
+  //Enemy Missiles
+  for(uint8_t i = 0; i < MAX_EMISSILES; i++){
+    //Check for a valid destination
+    if( eDests[i] <= 84 ){
+        gb.display.drawLine(eMissiles[i][0],eMissiles[i][1],eMissiles[i][2], eMissiles[i][3]);
     }
   }
 }
@@ -112,7 +130,34 @@ void launchMissile(uint8_t launcher){
   }
 }
 
+void tryLaunchEnemy(){
+  uint8_t someActive = 0;
+  // Check whether there are any active missiles
+  // If none are active, always spawn one to avoid
+  // long pauses without any enemy missiles
+  for(uint8_t i = 0; i < MAX_EMISSILES; i++){
+    if( eDests[i] <= 84 ){
+      someActive = 1;
+      break;
+    }
+  }
+  
+  if( etotal > 0 && (!someActive || echance >= random(100)) ){ //echance of 100
+    for(uint8_t i = 0; i < MAX_EMISSILES; i++){
+      if( eDests[i] > 84 ){
+        eDests[i] = random(8); //Target one of the 6 cities or 2 launch sites
+        eMissiles[i][0] = random(84); //Screen width
+        eMissiles[i][1] = 0; //Top of screen
+        eMissiles[i][2] = eMissiles[i][0]; //Start and end are same
+        eMissiles[i][3] = 0; //Top of screen
+        break; //Only spawn one
+      }
+    }
+  }
+}
+
 void stepMissiles(){
+  //Player Missiles
   for(uint8_t i = 0; i < MAX_PMISSILES; i++){
     //Check for a valid destination without a current detonation
     if( pDests[i][0] <= 84 && pDetonations[i][0] > 84 ){
@@ -127,6 +172,23 @@ void stepMissiles(){
         float dir = atan2(pDests[i][1]-pMissiles[i][1], pDests[i][0]-pMissiles[i][0]);
         pMissiles[i][0] += PSPEED * cos(dir);
         pMissiles[i][1] += PSPEED * sin(dir);
+      }
+    }
+  }
+
+  //Enemy Missiles
+  for(uint8_t i = 0; i < MAX_EMISSILES; i++){
+    //Check for a valid destination
+    if( eDests[i] <= 84 ){
+       //If enemy missile is close enough to the destination, detonate
+      if( abs( (eDests[i]*10+6) - eMissiles[i][2] ) < PSPEED && abs( 44 - eMissiles[i][3] ) < PSPEED ){
+        eDests[i] = 100;
+        //TODO: Destroy cities, launchers
+      //Otherwise, keep moving towards destination
+      }else{
+        float dir = atan2( 44-eMissiles[i][3], (eDests[i]*10+6)-eMissiles[i][2] );
+        eMissiles[i][2] += espeed * cos(dir);
+        eMissiles[i][3] += espeed * sin(dir);
       }
     }
   }
@@ -159,6 +221,19 @@ void stepDetonations(){
   }
 }
 
+void stepCollision(){
+  //Player Detonations with Enemy Missiles
+  for(uint8_t i = 0; i < MAX_PMISSILES; i++){
+      if( pDetonations[i][0] <= 84 ){
+        for(uint8_t j = 0; j < MAX_EMISSILES; j++){
+          if( pDetonations[i][2] >= sqrt( (eMissiles[j][2]-pDetonations[i][0])*(eMissiles[j][2]-pDetonations[i][0]) + (eMissiles[j][3]-pDetonations[i][1])*(eMissiles[j][3]-pDetonations[i][1]) ) ){
+            eDests[j] = 100; //Remove enemy missile
+          }
+        }
+      }
+  }
+}
+
 void loop() {
   if(gb.update()){
     oddLoop++;
@@ -186,8 +261,10 @@ void loop() {
     }
 
     //Game logic
+    tryLaunchEnemy();
     stepMissiles();
     stepDetonations();
+    stepCollision();
 
     //Drawing
     drawTargets();
